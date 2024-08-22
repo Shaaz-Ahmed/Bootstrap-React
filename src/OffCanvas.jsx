122b047998545d17894fb4c1bdc0f4e7b3367aa0
Shaaz-Ahmed <shaazmukadam1616@gmail.com>
2024-08-23 00:53:59 +0530

    added offcanvas

--- src/OffCanvas.jsx
+++ src/OffCanvas.jsx
@@ -1,19 +1,37 @@
 import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 const OffCanvas = () => {
   return (
     <>
-      <button className="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasTop" aria-controls="offcanvasTop">
-        Toggle top offcanvas
+      <button 
+        className="btn btn-primary" 
+        type="button" 
+        data-bs-toggle="offcanvas" 
+        data-bs-target="#offcanvasRight" 
+        aria-controls="offcanvasRight"
+      >
+        Toggle right offcanvas
       </button>
 
-      <div className="offcanvas offcanvas-top" tabIndex="-1" id="offcanvasTop" aria-labelledby="offcanvasTopLabel">
+      <div 
+        className="offcanvas offcanvas-end" 
+        tabIndex="-1" 
+        id="offcanvasRight" 
+        aria-labelledby="offcanvasRightLabel"
+      >
         <div className="offcanvas-header">
-          <h5 id="offcanvasTopLabel">Offcanvas top</h5>
-          <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+          <h5 id="offcanvasRightLabel">Offcanvas right</h5>
+          <button 
+            type="button" 
+            className="btn-close text-reset" 
+            data-bs-dismiss="offcanvas" 
+            aria-label="Close"
+          ></button>
         </div>
         <div className="offcanvas-body">
-          hewfgdf dbudbvhvdq hdkqcbhk chvqds advvg vdhv u fdnj hfg fvfo
+          ...
         </div>
       </div>
     </>
