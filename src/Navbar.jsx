89b1020b85c947d9b42ada8966c94693de3e6dde
Shaaz-Ahmed <shaazmukadam1616@gmail.com>
2024-08-22 23:58:36 +0530

    added dropdown mwnu

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
                   Dropdown
                 </a>
                 <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <li><a className="dropdown-item" href="#">Action</a></li>
+                  <li><a className="dropdown-item" href="#">ActionS</a></li>
                   <li><a className="dropdown-item" href="#">Another action</a></li>
                   <li><hr className="dropdown-divider" /></li>
                   <li><a className="dropdown-item" href="#">Something else here</a></li>
