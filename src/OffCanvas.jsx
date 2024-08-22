3691061a70c0fb4d7d9f7d88702632358a77dca3
Shaaz-Ahmed <shaazmukadam1616@gmail.com>
2024-08-23 00:57:50 +0530

    canvas is like onclick or hidden sidebar

--- src/OffCanvas.jsx
+++ src/OffCanvas.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 const OffCanvas = () => {
   return (
     <>
+      {/* Button to toggle right OffCanvas */}
       <button 
         className="btn btn-primary" 
         type="button" 
@@ -15,6 +16,7 @@ const OffCanvas = () => {
         Toggle right offcanvas
       </button>
 
+      {/* Right OffCanvas */}
       <div 
         className="offcanvas offcanvas-end" 
         tabIndex="-1" 
@@ -32,6 +34,38 @@ const OffCanvas = () => {
         </div>
         <div className="offcanvas-body">
           hehehehehe hehehe ehellloo heheheheh
+        </div>
+      </div>
+
+      {/* Button to toggle top OffCanvas */}
+      <button 
+        className="btn btn-primary" 
+        type="button" 
+        data-bs-toggle="offcanvas" 
+        data-bs-target="#offcanvasTop" 
+        aria-controls="offcanvasTop"
+      >
+        Toggle top offcanvas
+      </button>
+
+      {/* Top OffCanvas */}
+      <div 
+        className="offcanvas offcanvas-top" 
+        tabIndex="-1" 
+        id="offcanvasTop" 
+        aria-labelledby="offcanvasTopLabel"
+      >
+        <div className="offcanvas-header">
+          <h5 id="offcanvasTopLabel">Offcanvas top</h5>
+          <button 
+            type="button" 
+            className="btn-close text-reset" 
+            data-bs-dismiss="offcanvas" 
+            aria-label="Close"
+          ></button>
+        </div>
+        <div className="offcanvas-body">
+          hehehe hehe eheh ehehe heheheheh
         </div>
       </div>
     </>
