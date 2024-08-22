d59bd50b1cca866858eb06c69fd076f7416209b3
Shaaz-Ahmed <shaazmukadam1616@gmail.com>
2024-08-23 00:20:52 +0530

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Cards from './Cards';
+import Navbar from './Navbar';
 
 const App = () => {
   return (
     <div>
+      <Navbar/> 
       <Cards/>
+     
     </div>
   );
 }
